545c17a886506aa594c485737da4f5921298decf
aung123ko <phyoaung978j@gmail.com>
2025-10-21 06:31:56 +0100

    Update black_donut_sim_ros2_control.launch.py

---
+++ black_donut/black_donut_gazebo/launch/black_donut_sim_ros2_control.launch.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessExit, OnProcessStart
+import xacro
+
+def generate_launch_description():
+    gazebo_pkg = get_package_share_directory('black_donut_gazebo')
+    description_pkg = get_package_share_directory('black_donut_description')
+    default_world_path = os.path.join(gazebo_pkg, 'worlds', 'empty.world')
+
+    
+    bot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            description_pkg, 'launch', 'description_ros2_control.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            gazebo_pkg,'launch','controller_spawner.launch.py'
+        )]),
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(description_pkg, 'rviz2', 'black_donut.rviz')],
+        condition=IfCondition(LaunchConfiguration('open_rviz'))
+    )
+
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory("gazebo_ros"), "launch", "gazebo.launch.py")),
+        launch_arguments={
+            "use_sim_time": "true",
+            "robot_name": "black_donut",
+            "world": "default_world_path",
+            "lite": "false",
+            "world_init_x": "0.0",
+            "world_init_y": "0.0",
+            "world_init_heading": "0.0",
+            "gui": "true",
+            "close_loop_odom": "true",
+        }.items(),
+    )
+
+    spawn_robot_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        # arguments=['-database', 'rom2109_tall_ros', '-entity', 'rom2109_tall_ros',
+        arguments=['-entity', 'black_donut', '-topic', '/robot_description',
+                   "-x", '0.0',
+                   "-y", '0.0',
+                   "-z", '0.3'],
+        output='screen'
+    )
+              
+
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument('open_rviz', default_value='false', description='Open RViz.'),
+            gazebo_launch,
+            spawn_robot_node,
+            bot,
+            rviz_node,
+            controller,
+        ]
+    )
